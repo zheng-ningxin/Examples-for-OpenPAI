9cc827da3711bcb4315d6b58296290b5041be46f
NingxinZheng <Ningxin.Zheng@microsoft.com>
2020-04-09 04:50:21 -0700

    Add Learning rate decay.
    Currently only add the Multistep learning rate decay.
    
    Signed-off-by: NingxinZheng <Ningxin.Zheng@microsoft.com>

--- cifar.py
+++ cifar.py
@@ -28,10 +28,12 @@ def parse_args():
     parser.add_argument('--epoch', type=int, default=200, help='The number of epochs')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum value for optimizer')
     parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay for the optimizer')
-    
+    #parser.add_argument('--lr_decay', choices=[None, 'cos', 'stage', 'step'], default=None, help='Learing rate decay')
+
     parser.add_argument('--cpu', default=False, action='store_true', help='Only use CPU to train')
     parser.add_argument('--gpuid', default='0', type=str, help='Gpus used for training')
     parser.add_argument('--arch', choices=support_models, default='ResNet18', help='Network type used for training')
+    parser.add_argument('--outdir', type=str, default='./log', help='Outdir of results')
     return parser.parse_args()
 
 
@@ -47,8 +49,8 @@ def build_net(args):
         args.gpu =False
         return net
     args.gpu = True
-    gpus = list(filter(lambda x:len(x)>0, re.split(',', args.gpuid)))
-    if len(gpus) > 1:
+    args.gpus = list(filter(lambda x:len(x)>0, re.split(',', args.gpuid)))
+    if len(args.gpus) > 1:
         net = nn.DataParallel(net)
     return net.cuda()
 
@@ -72,7 +74,7 @@ def train_epoch(net, train_loader, optimizer, args):
         count += data.size(0)
         _, predict = output.max(1)
         correct += predict.eq(target).sum().item()
-    print('Loss: %.2f , Accuracy: %.2f' %(loss_sum/len(train_loader), correct/count) )
+    print('Loss: %.3f , Accuracy: %.3f' %(loss_sum/len(train_loader), correct/count) )
 
 
 def validate(net, data_loader, args):
@@ -90,7 +92,8 @@ def validate(net, data_loader, args):
             loss_sum += loss.item()
             count += target.size(0)
             correct += predict.eq(target).sum().item()
-        print('Loss: %.2f  Accuracy: %2f' % (loss_sum/len(data_loader), correct/count))
+        print('Loss: %.3f  Accuracy: %.3f' % (loss_sum/len(data_loader), correct/count))
+    return correct/count
 
 
 def prepare_data(args):
@@ -106,7 +109,7 @@ def prepare_data(args):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
     train_set = torchvision.datasets.CIFAR10(root='./data', transform=cifar_transform_train, train=True, download=True)
-    val_set = torchvision.datasets.CIFAR10(root='./data', transform=cifar_transform_val, train=False)
+    val_set = torchvision.datasets.CIFAR10(root='./data', transform=cifar_transform_val, train=False, download=True)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batchsize, shuffle=True, num_workers=4)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=args.batchsize, shuffle=False, num_workers=4)
     return train_loader, val_loader
@@ -116,12 +119,26 @@ def train(args):
     train_loader, val_loader = prepare_data(args)
     net = build_net(args)
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(args.epoch*0.5), int(args.epoch*0.75)], gamma=0.1)
+    best_acc = 0
+    checkpoint = {}
     for epochid in range(args.epoch):
-        print("==> Training Epoch %d" % epochid)
+        print("==> Training Epoch %d, Learning Rate %.4f" % (epochid, lr_scheduler.get_lr()[0]))
         train_epoch(net, train_loader, optimizer, args)
         print('==> Validating ')
-        validate(net, val_loader, args)
-    
+        acc = validate(net, val_loader, args)
+        lr_scheduler.step()
+        if acc > best_acc:
+            best_acc = acc
+            if len(args.gpus) > 1:
+                checkpoint = net.module.state_dict()
+            else:
+                checkpoint = net.state_dict()
+    fname = args.arch + '.pth.tar'
+    os.makedirs(args.outdir, exist_ok=True)
+    fname = os.path.join(args.outdir, fname)
+    torch.save(checkpoint, fname)
+    print('Best Accuracy: ', best_acc)
 
 if __name__ == '__main__':
     args = parse_args()
